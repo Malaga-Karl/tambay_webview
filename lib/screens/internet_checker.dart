1720f282ed3bf1a7fb68991f26486ed5d6e5bb6e
Malaga-Karl <kbkmalaga2021@plm.edu.ph>
2025-07-07 18:13:37 +0800

    network changed fix

--- lib/screens/internet_checker.dart
+++ lib/screens/internet_checker.dart
@@ -1,8 +1,8 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 import 'package:tambay_webview/screens/no_internet_screen.dart';
-import 'package:tambay_webview/screens/webview_screen.dart';
 
 class ConnectionChecker extends StatefulWidget {
   const ConnectionChecker({super.key});
@@ -14,16 +14,27 @@ class ConnectionChecker extends StatefulWidget {
 class _ConnectionCheckerState extends State<ConnectionChecker> {
   late final StreamSubscription<List<ConnectivityResult>> subscription;
   bool hasInternet = false;
+  late final WebViewController _controller;
+  bool _webViewInitialized = false;
 
   @override
   void initState() {
     super.initState();
+    _controller =
+        WebViewController()
+          ..setJavaScriptMode(JavaScriptMode.unrestricted)
+          ..loadRequest(Uri.parse('https://tambay.co'));
     _checkConnection();
     subscription = Connectivity().onConnectivityChanged.listen((results) {
+      final connected = results.any(
+        (result) => result != ConnectivityResult.none,
+      );
+      if (connected && !hasInternet && _webViewInitialized) {
+        // If internet is restored, reload the page
+        _controller.reload();
+      }
       setState(() {
-        hasInternet = results.any(
-          (result) => result != ConnectivityResult.none,
-        );
+        hasInternet = connected;
       });
     });
   }
@@ -44,7 +55,9 @@ class _ConnectionCheckerState extends State<ConnectionChecker> {
   @override
   Widget build(BuildContext context) {
     if (hasInternet) {
-      return const WebViewScreen();
+      return Scaffold(
+        body: SafeArea(child: WebViewWidget(controller: _controller)),
+      );
     } else {
       return const NoInternetScreen();
     }
